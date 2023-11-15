827ced0b13344e7bad5b4b8ab0a2ed7424c8b5a0
giordano.daloisio <giordano.daloisio@gmail.com>
2023-11-15 17:12:25 +0100

    changed with sphinx project

---
+++ source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "MANILA"
+copyright = "2023, Giordano d'Aloisio"
+author = "Giordano d'Aloisio"
+release = "0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser"]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
--- tutorial.md
+++
@@ -1,4 +0,0 @@
----
-title: Tutorial
-permalink: /tutorial
----
